2347a864cc1480b5b877c013e91c338d1e503cdb
99002616 <71481308+99002616@users.noreply.github.com>
2020-10-31 16:42:19 +0530

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,58 @@
+class Customer{
+	constructor(name,email,billdate,phone,billamount){
+		this.name=name;
+		this.email=email;
+		this.billdate=billdate;
+		this.phone=phone;
+		this.billamount=billamount;
+	}
+	
+}
+function employeevalidation()
+{
+	this.name!=[@,#,$];
+}
+}
+
+const getDate = function(dt){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const year = dt.getFullYear();
+    const month = months[dt.getMonth()];
+    const day = dt.getDate();
+    
+  if (this.phone!==sizeof(10))
+  {
+  	throw ("invalid phone number");
+  }
+
+  if(this.billamount<500)
+  {
+  	throw ("Bill amount should be minimum of rupees 500");
+  }
+  function validate(){
+            //get all the values....
+            let name = document.regForm.name.value;
+            let email = document.regForm.email.value;
+            let mobile = document.regForm.phone.value;
+          
+            //name validation
+            if(name == "" || name == null){
+                showErr("errName", "Name is mandatory")
+                return false;
+            }else{
+                showErr("errName", "")
+            }
+            //email validation    
+            if(email == ""){
+                showErr("errEmail", "Email is mandatory")
+                return false;
+            }else{
+                showErr("errEmail", "")
+            }
+            let regEx = new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+            if(regEx.test(email) == false){
+                showErr("errEmail", "Email is not in a valid format")
+                return false;
+            } else {
+                showErr("errEmail", "")
+            }
